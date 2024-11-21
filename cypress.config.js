6b56bb37c1cace1b7dcaf86970fc872386aa678c
fazleyazdan <fazleyazdan345@gmail.com>
2024-11-21 17:05:03 +0500

    new config added

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,6 @@ module.exports = defineConfig({
       // implement node event listeners here
       on('file:preprocessor', cucumber())
     },
-    specPattern: "cypress/e2e/*.feature",
+    specPattern: "cypress/cucumber-bdd/**/*.feature",
   },
 });
